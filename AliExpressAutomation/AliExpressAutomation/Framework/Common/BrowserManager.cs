3da264b991ef521284a8f8852c08b2e0e965018c
Rodrigo.Zarate <zarate.ro@gmail.com>
2021-07-12 23:18:07 -0400

    Adding Logic for BrowserManager and DriverManager + App.config

--- AliExpressAutomation/AliExpressAutomation/Framework/Common/BrowserManager.cs
+++ AliExpressAutomation/AliExpressAutomation/Framework/Common/BrowserManager.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AliExpressAutomation.Framework.Common
 {
-    class BrowserManager
+    /// <summary>
+    /// The browser manager.
+    /// </summary>
+    public class BrowserManager
     {
+        /// <summary>
+        /// The singleton browser manager instance.
+        /// </summary>
+        private static BrowserManager _instance;
+
+        /// <summary>
+        /// The web driver.
+        /// </summary>
+        public IWebDriver WebDriver { get; private set; }
+
+        /// <summary>
+        /// The URL
+        /// </summary>
+        public string Url => ConfigurationManager.AppSettings["webUrl"];
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="browser">The browser name.</param>
+        private BrowserManager()
+        {
+            _instance = new BrowserManager();
+        }
+
+        /// <summary>
+        /// Gets the current browser manager instance.
+        /// </summary>
+        /// <returns>The browser manager.</returns>
+        public static BrowserManager Instance
+        {
+            get
+            {
+                return _instance = _instance == null ? new BrowserManager() : _instance;
+            }
+        }
+
+        /// <summary>
+        /// Open a web browser.
+        /// </summary>
+        public void OpenBrowser()
+        {
+            WebDriver = new DriverManager().DriverFactory();
+            WebDriver.Manage().Window.Maximize();
+            WebDriver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        /// <summary>
+        /// Method to navigate to URL
+        /// </summary>
+        public void GoTo()
+        {
+            WebDriver.Navigate().GoToUrl(Url);
+        }
+
     }
 }
---
+++ AliExpressAutomation/AliExpressAutomation/Framework/Common/DriverManager.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliExpressAutomation.Framework.Common
+{
+    /// <summary>
+    /// Driver Manager Class
+    /// </summary>
+    public class DriverManager
+    {
+        /// <summary>
+        /// Method to create Driver Instance
+        /// </summary>
+        public IWebDriver DriverFactory()
+        {
+            IWebDriver instance;
+            string driverVersion = ConfigurationManager.AppSettings["BrowserToExecuteTests"];
+
+            switch (driverVersion)
+            {
+                case "Firefox":
+                    instance = new FirefoxDriver();
+                    break;
+                case "Chrome":
+                    instance = new ChromeDriver();
+                    break;
+                case "IE":
+                    instance = new InternetExplorerDriver();
+                    break;
+                case "Edge":
+                    instance = new EdgeDriver();
+                    break;
+                default:
+                    instance = new ChromeDriver();
+                    break;
+            }
+
+            return instance;
+        }
+    }
+}
